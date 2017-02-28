d457f4b04adac33dd710809942e5f6fc6abb64d9
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2017-02-27 23:36:34 -0400

    Recursion examples, didnt get pieces :(

---
+++ recursion.erl
@@ -0,0 +1,17 @@
+-module(recursion).
+-export([fib/1,pieces/1]).
+
+fib(0) ->
+    0;
+fib(1) ->
+    1;
+fib(N) ->
+    fib(N-1) + fib(N-2).
+
+pieces(0) ->
+    1;
+pieces(N) ->
+    N + pieces(N-1).
+
+
+
